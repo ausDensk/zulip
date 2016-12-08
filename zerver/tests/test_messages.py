d4995420c369e102927d16c68c80e70b69f4c9b4
ausDensk <dshingis.qun@gmail.com>
2016-12-08 22:34:35 +0100

    Angegebene Tests eingefügt

--- zerver/tests/test_messages.py
+++ zerver/tests/test_messages.py
@@ -1575,3 +1575,22 @@ class CheckMessageTest(ZulipTestCase):
         self.assertEqual(new_count, old_count + 2)
         self.assertEqual(ret['message'].sender.email, 'othello-bot@zulip.com')
         self.assertIn("there are no subscribers to that stream", most_recent_message(parent).content)
+
+class CrocodileTest(ZulipTestCase):
+    def test_change_bye_message(self):
+        # type: () -> None
+        sender = get_user_profile_by_email('othello@zulip.com')
+        client = make_client(name="test suite")
+        message_id = check_send_message(sender, client, "stream", ["Verona"], "Crocodile test", "bye")
+        self.assertEqual(
+            Message.objects.values_list("content", flat=True).get(id=message_id),
+            "See you in a while :crocodile:!")
+
+    def test_leave_hi_message_alone(self):
+        # type: () -> None
+        sender = get_user_profile_by_email('othello@zulip.com')
+        client = make_client(name="test suite")
+        message_id = check_send_message(sender, client, "stream", ["Verona"], "Crocodile test", "Hi there")
+        self.assertEqual(
+            Message.objects.values_list("content", flat=True).get(id=message_id),
+            "Hi there")
