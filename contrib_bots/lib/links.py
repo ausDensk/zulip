0e273bc63ef05a629e68e5784a8442c6bff68527
ausDensk <dshingis.qun@gmail.com>
2016-12-14 18:14:58 +0100

    Cloned followup bot

---
+++ contrib_bots/lib/links.py
@@ -0,0 +1,42 @@
+# See readme.md for instructions on running this code.
+
+class LinksHandler(object):
+    '''
+    This plugin facilitates creating a list of resources you want to save while using Zulip. It looks for messages starting with "@link" or "@resource". In this example, we send resources to private messages.
+    '''
+
+    def usage(self):
+        return '''
+            This plugin will allow users to flag messages as being resources and store them in private messages with the bot. Users should preface messages with "@link" or "@resource".
+            '''
+
+    def triage_message(self, message):
+        # return True iff we want to (possibly) response to this message
+
+        original_content = message['content']
+
+        # This next line of code is defensive, as we
+        # never want to get into an infinite loop of posting follow
+        # ups for own follow ups!
+        if message['display_recipient'] == 'links':
+            return False
+        is_follow_up = (original_content.startswith('@link') or
+                        original_content.startswith('@resource'))
+
+        return is_follow_up
+
+    def handle_message(self, message, client, state_handler):
+        original_content = message['content']
+        original_sender = message['sender_email']
+        if original_content.startsWith('@link'):
+            new_content = original_content.replace('@link', 'from %s:' % (original_sender,))
+        else:
+            new_content = original_content.replace('@resource', 'from %s:' % (original_sender,))
+        client.send_message(dict(
+            type='stream',
+            to='followup',
+            subject=message['sender_email'],
+            content=new_content,
+        ))
+
+handler_class = LinksHandler
