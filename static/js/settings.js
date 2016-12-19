343b8016caa46d15980f78ac1e841292b836077c
ausDensk <dshingis.qun@gmail.com>
2016-12-19 21:08:43 +0100

    Removed return statement

--- static/js/settings.js
+++ static/js/settings.js
@@ -531,7 +531,7 @@ function _setup_page() {
         loading.make_indicator(spinner, {text: 'Uploading avatar.'});
         
         console.log(form_data);
-        return channel.put({
+        channel.put({
             url:      '/json/users/me/avatar',
             idempotent: true,
             data:     form_data,
