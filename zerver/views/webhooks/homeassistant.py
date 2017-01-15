e97262ee2fc0e651eed6fd7ea4709974644fa250
ausDensk <dshingis.qun@gmail.com>
2017-01-15 23:14:58 +0100

    Something wrong...

--- zerver/views/webhooks/homeassistant.py
+++ zerver/views/webhooks/homeassistant.py
@@ -22,7 +22,7 @@ def api_homeassistant_webhook(request, user_profile, client,
         print(payload.message)
         body = payload.message
     except KeyError as e:
-        print("Irgendwas läuft nicht -.-")
+        print("Irgendwas laeuft nicht")
         return json_error(_("Missing key {} in JSON").format(str(e)))
 
     check_send_message(user_profile, client, 'stream', [stream], topic, body)
